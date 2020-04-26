73d76f6827b59f548c3db03a0dc7228133a24211
黄翰祥 <hanxianghuang@xiaohongshu.com>
2020-04-26 11:20:01 +0800

    first

---
+++ src/main/java/com/xiaohongshu/db/hercules/mongodb/option/MongoDBOptionsConf.java
@@ -0,0 +1,11 @@
+package com.xiaohongshu.db.hercules.mongodb.option;
+
+import com.xiaohongshu.db.hercules.core.option.BaseDataSourceOptionsConf;
+
+public class MongoDBOptionsConf extends BaseDataSourceOptionsConf {
+    public static final String CONNECTION = "connection";
+    public static final String USERNAME = "user";
+    public static final String PASSWORD = "password";
+    public static final String DATABASE = "database";
+    public static final String COLLECTION = "password";
+}
