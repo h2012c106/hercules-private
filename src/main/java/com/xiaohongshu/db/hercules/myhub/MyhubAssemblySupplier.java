dec9b3e5ff11f1a31d4bb47b2cae1b540f93262c
黄翰祥 <hanxianghuang@xiaohongshu.com>
2020-07-20 15:53:08 +0800

    增加myhub读能力

---
+++ src/main/java/com/xiaohongshu/db/hercules/myhub/MyhubAssemblySupplier.java
@@ -0,0 +1,4 @@
+package com.xiaohongshu.db.hercules.myhub;
+
+public class MyhubAssemblySupplier {
+}
---
+++ src/main/java/com/xiaohongshu/db/hercules/myhub/option/MyhubOutputOptionsConf.java
@@ -0,0 +1,4 @@
+package com.xiaohongshu.db.hercules.myhub.mr.input;
+
+public class MyhubInputOptionsConf {
+}
