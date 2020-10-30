95cdf63743576783fa4eb9a909e0f784a91da432
hqiu1 <hqiu1@xiaohongshu.com>
2020-10-30 19:38:37 +0800

    add redis type

--- command/redis/src/main/java/com/xiaohongshu/db/hercules/redis/action/InsertAction.java
+++ command/redis/src/main/java/com/xiaohongshu/db/hercules/redis/action/InsertAction.java
@@ -16,24 +16,34 @@ import java.util.Map;
  */
 public class InsertAction implements WriteAction{
 
-    public void act(Pipeline pipeline, RedisKV kv, Integer expire) {
+    public void act(Pipeline pipeline, RedisKV kv, Integer expire, String writeType) {
         String key = String.valueOf(kv.getKey().getValue());
         RedisKV.RedisKVValue value = kv.getValue();
         if (key == null || value.getValue() == null) {
             return;
         }
-        switch ((BaseDataType) value.getDataType()) {
-            case STRING:
+        switch (writeType.toLowerCase()) {
+            case "string":
                 String stringValue = String.valueOf(value.getValue());
                 pipeline.set(key, stringValue);
                 break;
-            case LIST:
-                throw new RuntimeException("The type List is not supported by redis.");
-            case MAP:
+            case "list":
                 byte[] key_byte = SafeEncoder.encode(key);
-                Map<byte[], byte[]> mapValue = (Map<byte[], byte[]>) (value.getValue());
-                pipeline.hmset(key_byte, mapValue);
+                byte[] value_byte = SafeEncoder.encode(String.valueOf(value.getValue()));
+                pipeline.rpush(key_byte, value_byte);
                 break;
+            case "hash":
+                byte[] key_byte_hash = SafeEncoder.encode(key);
+                Map<byte[], byte[]> mapValue = (Map<byte[], byte[]>) (value.getValue());
+                pipeline.hmset(key_byte_hash, mapValue);
+                break;
+            case "set":
+                byte[] key_byte_set = SafeEncoder.encode(key);
+                byte[] value_byte_set = SafeEncoder.encode(String.valueOf(value.getValue()));
+                pipeline.sadd(key_byte_set, value_byte_set);
+                break;
+            case "zset":
+                throw new RuntimeException("The type zset is not supported yet.");
             default:
                 throw new RuntimeException(String.format("The type [%s] is not supported by redis.", value.getDataType().toString()));
         }
